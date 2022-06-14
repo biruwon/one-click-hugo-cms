803b423e10f9ce0227c3c0579a2fef2085e269f5
Antonio Jesús <antonio.rodriguez@netlify.com>
2022-06-14 11:55:18 +0200

    Update hello-world.js

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,4 +1,9 @@
 exports.handler = async function (event, context) {
+
+  setTimeout(() => { 
+    console.log("I am here")
+  }, 20000);
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello functions origin" }),
