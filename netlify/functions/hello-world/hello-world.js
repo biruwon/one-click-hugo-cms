8cc08b805b927c93ab8efdf1b7afab3e7bbd3995
Antonio Jesús <antonio.rodriguez@netlify.com>
2023-02-17 12:46:38 +0100

    Update hello-world.js

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -5,11 +5,9 @@ const handler = async (event) => {
 
     console.log("This is me logging things!")
 
-    const value = process.env.AWS_LAMBDA_JS_RUNTIME;
-
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject} from functions origin. Value of AWS_LAMBDA_JS_RUNTIME is ${value}.` }),
+      body: JSON.stringify({ message: `Hello ${subject} from functions origin.` }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
