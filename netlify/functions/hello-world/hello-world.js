938c25b0bb93bdd4393b96861d158b89591b968e
Antonio Jesús <antonio.rodriguez@netlify.com>
2023-05-26 12:08:07 +0200

    Update hello-world.js

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -4,6 +4,8 @@ const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
 
+    console.log("This is me logging things!");
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject} from functions origin.` }),
