0396a71c3496f775ef257ab210ae733c30ea2e49
Antonio Jesús <antonio.rodriguez@netlify.com>
2024-01-24 16:21:40 +0100

    Create layer-background.js

---
+++ netlify/functions/layer-background.js
@@ -0,0 +1,15 @@
+// Trigger deploy 3
+const handler = async (event) => {
+  try {
+    console.log("This is extra function!")
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `This is extra function!` }),
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
